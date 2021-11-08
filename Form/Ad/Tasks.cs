afaecff22ce97dd17694250167b2d3e662e6b812
Bleach17 <antosha.chugunov@list.ru>
2021-11-08 09:29:43 +0300

    form v1

---
+++ Form/Ad/Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Form.Pages
+{
+    public class Tasks
+    {
+        public int Name { get; set; }
+
+        public bool Check { get; set; }
+    }
+}
